a06475c3efa59385f0f129b93f497fb2d69d3836
MLuzzy <mickael.filipe@edu.itescia.fr>
2021-03-19 15:41:54 +0100

    1er commit

---
+++ src/main/java/com/team/congefacile/CongefacileApplication.java
@@ -0,0 +1,13 @@
+package com.team.congefacile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CongefacileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CongefacileApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/team/congefacile/CongefacileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team.congefacile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CongefacileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
